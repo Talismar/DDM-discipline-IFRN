a9f05dc957aec1588336baa9fcf8196a103301c5
Talismar <talismar788.una@gmail.com>
2023-02-14 20:13:12 -0300

    Removing the projectInitial folder

--- src/screens/NewTask/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components/native";
-
-export const Container = styled.View`
-  flex: 1;
-  background-color: ${({ theme }) => theme.COLORS.WHITE};
-  align-items: center;
-`;
-
-export const Title = styled.Text``;
